4b748bce1c80638737c487b3a315f6c637d6b629
Maxime Van Eygen <maximevaneygen@gmail.com>
2023-04-12 13:39:13 +0200

    add put

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -18,6 +18,16 @@ class NotesController < ApplicationController
         @note.destroy
         redirect_to notes_path
     end
+
+    def edit
+        @note = Note.find(params[:id])
+    end 
+
+    def update
+        @note = Note.find(params[:id])
+        @note.update(note_params)
+        redirect_to notes_path
+    end
     
     private
     def note_params
